0d94a7444d0b8fd706a1de2fb66e3b023b381eac
sbriones97 <sbriones97@hotmail.com>
2019-03-20 18:47:26 -0600

    Add Commands according to requirements

--- MIPSProcessor_ver1/Sources/ALUControl.v
+++ MIPSProcessor_ver1/Sources/ALUControl.v
@@ -20,13 +20,17 @@ module ALUControl
 
 );
 
+
 localparam R_Type_AND  = 9'b111_100100;
 localparam R_Type_OR   = 9'b111_100101;
 localparam R_Type_NOR  = 9'b111_100111;
-localparam R_Type_ADD  = 9'b111_100000;
+localparam R_Type_ADD  = 9'b111_010100;
+localparam R_Type_SLL  = 9'b111_000000;
+localparam R_Type_SRL  = 9'b111_000010;
 localparam I_Type_ADDI = 9'b100_xxxxxx;
 localparam I_Type_ORI  = 9'b101_xxxxxx;
 localparam I_Type_LUI  = 9'b011_xxxxxx;
+localparam I_Type_ANDI  = 9'b101_xxxxxx;
 
 reg [3:0] ALUControlValues;
 wire [8:0] Selector;
@@ -37,12 +41,15 @@ always@(Selector)begin
 	casex(Selector)
 		R_Type_AND:    ALUControlValues = 4'b0000;
 		R_Type_OR: 		ALUControlValues = 4'b0001;
-		I_Type_ORI:		ALUControlValues = 4'b0001;
 		R_Type_NOR:		ALUControlValues = 4'b0010;
 		R_Type_ADD:		ALUControlValues = 4'b0011;
-		I_Type_ADDI:	ALUControlValues = 4'b0011;
-		I_Type_LUI: 	ALUControlValues = 4'b0101;
-		default: ALUControlValues = 4'b1001;
+		R_Type_SLL:		ALUControlValues = 4'b0100;
+		R_Type_SRL:		ALUControlValues = 4'b0101;
+		I_Type_ADDI: 	ALUControlValues = 4'b0110;
+		I_Type_ORI: 	ALUControlValues = 4'b0111;
+		I_Type_LUI: 	ALUControlValues = 4'b1000;
+		I_Type_ANDI: 	ALUControlValues = 4'b1001;
+		default: ALUControlValues = 4'b1111;
 	endcase
 end
 
